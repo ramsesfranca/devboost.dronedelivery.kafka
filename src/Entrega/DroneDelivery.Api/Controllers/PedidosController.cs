da141070dc5e4b71624e1d6aa96aae09bb0b979a
Leandro Bevilaqua <le.bevilaqua@gmail.com>
2020-09-14 16:57:35 -0300

    remover endpoint criar pedido da entrega

--- src/Entrega/DroneDelivery.Api/Controllers/PedidosController.cs
+++ src/Entrega/DroneDelivery.Api/Controllers/PedidosController.cs
@@ -47,6 +47,7 @@ namespace DroneDelivery.Api.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ApiExplorerSettings(IgnoreApi = true)]
         public async Task<IActionResult> Adicionar(CriarPedidoCommand command)
         {
             var response = await EventBus.SendCommand(command);
